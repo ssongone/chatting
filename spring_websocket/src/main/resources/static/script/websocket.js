3f7b2f8cc277d6b051bde5c3f0f207924a77c409
ssongone <41146676+ssongone@users.noreply.github.com>
2023-05-22 21:20:48 +0900

    Feat: 송신자 인증 with 이름

--- spring_websocket/src/main/resources/static/script/websocket.js
+++ spring_websocket/src/main/resources/static/script/websocket.js
@@ -1,21 +1,38 @@
 const socket = new WebSocket("ws://localhost:8080/chat"); // 서버의 WebSocket 엔드포인트에 연결합니다.
+iam = "익명의 누군가";
 
 socket.onmessage = function(event) {
-    const message = event.data;
+    //const message = event.data;
+    const response = JSON.parse(event.data);
+    const sender = response.name;
+    const message = response.message;
+    const time = response.time;
+    tmp = ""
+    if (sender != "익명의 누군가" && sender == iam) {
+        tmp = "(나)"
+    }
+    console.log(name);
+    console.log(message);
+    console.log(time);
+
     const listItem = document.createElement("li");
-    listItem.textContent = message;
+    listItem.textContent = sender +": " + tmp + message;
+
     document.getElementById("messageList").appendChild(listItem);
 };
 
 document.getElementById("messageInput").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         const name = document.getElementById("name").value;
+        if (name!="") {
+            iam = name;
+        }
         const message = document.getElementById("messageInput").value;
         const time = new Date().toLocaleString();
 
         // JSON 객체 생성
         const data = {
-            name: name,
+            name: iam,
             message: message,
             time: time
         };
